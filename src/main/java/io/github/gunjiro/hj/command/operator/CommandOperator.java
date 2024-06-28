d9919674a4d08c8e52469788a7e6e951ffa0a8ac
Gunjiro <gunjiro.san@gmail.com>
2024-06-28 04:45:13 +0000

    Remove CommandOperator

--- src/main/java/io/github/gunjiro/hj/command/operator/CommandOperator.java
+++
@@ -1,8 +0,0 @@
-package io.github.gunjiro.hj.command.operator;
-
-import io.github.gunjiro.hj.ExitException;
-import io.github.gunjiro.hj.command.Command;
-
-public interface CommandOperator {
-    public void operate(Command command) throws ExitException;
-}
