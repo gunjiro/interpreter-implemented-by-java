9d85adbea6753a8759d8c014520f55b6ef5fa1e4
Gunjiro <gunjiro.san@gmail.com>
2024-07-03 15:28:13 +0000

    Create OutputOperation

---
+++ src/main/java/io/github/gunjiro/hj/ui/OutputOperation.java
@@ -0,0 +1,16 @@
+package io.github.gunjiro.hj.ui;
+
+public class OutputOperation {
+
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void printText(String text) {
+        System.out.print(text);
+    }
+
+    public void startANewLine() {
+        System.out.println();
+    }
+}
