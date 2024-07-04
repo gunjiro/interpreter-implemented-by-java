55a5b89f358445e8a4be02c967054c9824bc05cf
Gunjiro <gunjiro.san@gmail.com>
2024-07-04 13:01:59 +0000

    Remove App

--- src/main/java/io/github/gunjiro/hj/AppFactory.java
+++
@@ -1,6 +0,0 @@
-package io.github.gunjiro.hj;
-public class AppFactory {
-    public App create(InputReceiver receiver, ResourceProvider provider, StringPrinter stringPrinter, MessagePrinter messagePrinter, Environment environment) {
-        return new App(IOLoop.create(receiver, provider, stringPrinter, messagePrinter, environment), messagePrinter);
-    }
-}
