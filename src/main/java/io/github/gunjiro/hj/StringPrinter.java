9400c59d73660f321c961728c2eb632a90f643fe
Gunjiro <gunjiro.san@gmail.com>
2024-07-04 13:15:52 +0000

    Remove StringPrinter

--- src/main/java/io/github/gunjiro/hj/StringPrinter.java
+++
@@ -1,4 +0,0 @@
-package io.github.gunjiro.hj;
-public interface StringPrinter {
-    public void print(String s);
-}
--- src/main/java/io/github/gunjiro/hj/SystemOutStringPrinter.java
+++
@@ -1,7 +0,0 @@
-package io.github.gunjiro.hj;
-public class SystemOutStringPrinter implements StringPrinter {
-    @Override
-    public void print(String s) {
-        System.out.print(s);
-    }
-}
